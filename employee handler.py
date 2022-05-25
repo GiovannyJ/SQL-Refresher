a4c5b7a26cab9dc5bfa7f0d59ecc2fb715c732c5
GiovannyJ <94592434+GiovannyJ@users.noreply.github.com>
2022-05-24 23:42:12 -0400

    first commit

---
+++ employee handler.py
@@ -0,0 +1,108 @@
+import time
+import sqlite3
+
+#create connection to sqlite db
+connection = sqlite3.connect('Employee_info.db')
+cursor = connection.cursor()
+
+#! change length for id
+cursor.execute("CREATE TABLE IF NOT EXISTS EMPLOYEES(id n(5), fname char(15), lname char(15), pnum n(10), title char(10));")
+
+# create unique id that not in databse yet
+# !fix later
+def create_id():
+    id = time.time()
+    return id
+        
+    
+class employee:
+    def __init__(self, first_name: str, last_name: str, phone_number: int, title: str) -> None:
+        self.fname = first_name
+        self.lname = last_name
+        self.email = self.fname + self.lname + "@company.com"
+        self.pnum = phone_number
+        self.id = create_id()
+        self.title = title
+
+    def get_fname(self):
+        return self.fname
+
+    def get_lname(self):
+        return self.lname
+
+    def get_email(self):
+        return self.email
+    
+    def get_pnum(self):
+        return self.pnum
+
+    def get_id(self):
+        return self.id
+    
+    def get_title(self):
+        return self.title
+
+def add_employee(*e) -> None:
+    #iterate over given values and add each one to db
+    for arg in e:
+        executeStatement = "INSERT INTO EMPLOYEES VALUES (?, ?, ?, ?, ?)"
+        values = [arg.get_id(), arg.get_fname(), arg.get_lname(), arg.get_pnum(), arg.get_title()]
+        cursor.executemany(executeStatement, (values, ))
+        connection.commit()
+
+#! error handling if id dont exist
+def remove_employee(*e) -> None:
+    #iterate over values and remove each one
+    for arg in e:
+        # removing if arg is class type
+        if type(arg) == employee:
+            executeStatement = "DELETE FROM EMPLOYEES WHERE id = ?"
+            cursor.execute(executeStatement, (arg.get_id(), ))
+            connection.commit()
+        #rm if arg is number given by user
+        elif type(arg) == int or float:
+            executeStatement = "DELETE FROM EMPLOYEES WHERE id = ?"
+            cursor.execute(executeStatement, (arg, ))
+            connection.commit()
+        else:
+            print("error bro")
+
+def promote_employee(e, new_title: str) -> None:
+    if type(e) == employee:
+        executeStatement = "UPDATE EMPLOYEES set title = ? where id = ?"
+        cursor.execute(executeStatement, (new_title ,e.get_id()))
+        connection.commit()
+    elif type(e) == int or float:
+        executeStatement = "UPDATE EMPLOYEES set title = ? where id = ?"
+        cursor.execute(executeStatement, (new_title ,e))
+        connection.commit()
+    else:
+        print("error promo bromo")
+
+#?  add diff ways to search for user
+def display_employee(e):
+    if type(e) ==  employee:
+        executionStatement = "SELECT * FROM EMPLOYEES WHERE id = ?"
+        cursor.execute(executionStatement, (e.get_id(), ))
+        display = cursor.fetchall()
+        return display
+    elif type(e) == int or float:
+        executionStatement = "SELECT * FROM EMPLOYEES WHERE id = ?"
+        cursor.execute(executionStatement, (e, ))
+        display = cursor.fetchall()
+        return display
+
+
+
+def main():
+    employee1 = employee("Giovanny", "Joseph", 51739981, "Backend Dev")
+    employee2 = employee("Nicole", "Joseph", 282130922, "Manager")
+    employee3 = employee("Chris", "Lopez", 5168928777,  "Backend Dev")
+    employee4 = employee("Sarah", "Pietrafesa", 5164453823, "Frontend Dev")
+    employee5 = employee("Nikki", "Heat", 999999999, "CEO")
+
+    add_employee(employee1, employee2, employee3, employee4, employee5)
+
+
+if __name__ == '__main__':
+    main()
